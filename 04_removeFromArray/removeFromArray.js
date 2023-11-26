c9b516d6bbc560bce19ecaec208388ef2309039a
AYanovDev <alex.ianovskii@mail.ru>
2023-11-27 00:00:28 +0600

    Accomplished half the tasks

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+let targetArr = arr;
+for (let j = arguments.length - 1; j > 0; j--) {
+for (let i = arr.length - 1; i >= 0; i--) {
+if (targetArr[i] === arguments[j]) {
+    targetArr.splice([i], 1);   
+    // delete targetArr[i];
+}
+}
+}
+return(targetArr);
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
