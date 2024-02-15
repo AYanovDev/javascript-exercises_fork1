008fbe22852dfbbdb35729ddc324f29b22f774a0
AYanovDev <alex.ianovskii@mail.ru>
2024-02-15 16:59:46 +0600

    Finalized the solution to the last task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,33 +1,16 @@
-const people = [
-  {
-    name: "Carly",
-    yearOfBirth: 1942,
-    yearOfDeath: 1970,
-  },
-  {
-    name: "Ray",
-    yearOfBirth: 1962,
-    yearOfDeath: 2011,
-  },
-  {
-    name: "Jane",
-    yearOfBirth: 1912,
-    yearOfDeath: 1941,
-  },
-];
-
-const findTheOldest = function (obj) {
-  let result = [];
-  for (let i = 0; i < obj.length; i++) {
-    result.push(Object.values(obj[i]));
+const findTheOldest = function (arr) {
+  let yearsLived = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!("yearOfDeath" in arr[i])) {
+      arr[i].yearOfDeath = 2024;
+    }
+    let personLived = arr[i].yearOfDeath - arr[i].yearOfBirth;
+    yearsLived.push(personLived);
   }
-  let flattened = result.flat();
-  let regex = /[1-9][0-9][0-9][0-9]/;
-  let filtered = flattened.filter((item) => regex.test(item));
-  console.log(filtered);
+  let longestLiving = yearsLived.indexOf(Math.max(...yearsLived));
+  let result = { name: arr[longestLiving].name };
+  return result;
 };
-
-findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
